bed5c91af1f611a8c2eca014bcc408a20780ca0f
Mahmoud Khamis <7ode.mo7amed1599@gmail.com>
2023-10-14 20:11:37 +0300

    initial commit

---
+++ Hash/main.cpp
@@ -0,0 +1,11 @@
+#include "Hash.cpp"
+#include <iostream>
+using namespace std;
+int main() {
+  
+  Hash hash;
+  hash.Hash32("This is Original Text");
+  hash.Hash64("This is Original Text");
+
+  return 0;
+}
